616bb517d874c337da851a6523ff17a40fac45a2
Ryan Erricson <ryanerricson@gmail.com>
2021-03-18 16:49:18 -0400

    commit tree ed3db3cd396f73c1552a263be2fad30a93615d1b

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,12 +6,19 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if owner = Owner.find_by_id(params["pet"]["owner_id"])
+      @pet.owner = owner
+    else
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
@@ -19,8 +26,29 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
-
-    redirect to "pets/#{@pet.id}"
+  get '/pets/:id/edit' do 
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
   end
+
+  patch '/pets/:id' do
+    ####### bug fix
+    if !params[:pet].keys.include?("owner_id")
+    params[:pet]["owner_id"] = []
+    end
+    #######
+
+    @pet = Pet.find(params[:id])
+
+    @pet.name = params["pet"]["name"]
+    
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    else
+      @pet.owner = Owner.find_by_id(params["pet"]["owner_id"])
+    end
+    @pet.save
+    redirect "pets/#{@pet.id}"
+end
 end
